0600896d368f872cf4a1c82bccb9176182c4e54b
David854481 <107841443+David854481@users.noreply.github.com>
2022-06-27 14:56:25 +0900

    UI Layout + Screen Transitions
    Greybox moment of the UI Screens. No assets yet

---
+++ Assets/Scripts/CategoryStatic.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CategoryStatic : MonoBehaviour
+{
+    //Holds the current category set 
+    public static string CategoryInformation { get; set;}
+}
---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public void switchScene(int sceneID)
+    {
+        switch (sceneID)
+        {
+            case 0: //To MainMenu Scene
+                //Switches to the MainMenu screen
+                SceneManager.LoadScene("MainMenu");
+                break;
+            case 1: //To CategoryMenu Scene
+                //Switches to the Results screen
+                SceneManager.LoadScene("CategoryMenu");
+                break;
+            case 2: //To DoYouKnowFilipino Scene
+                //Sets category information for main game
+                CategoryStatic.CategoryInformation = "test"; //change test to a dynamic variable
+                //Switches to the MainGame screen
+                SceneManager.LoadScene("Do You Know Filipino");
+                break;
+            case 3: //To Results Screen
+                //Set the score to the playerpref or database something that holds the scores
+
+                //Switches to the Results screen
+                SceneManager.LoadScene("Results");
+                break;
+        }
+    }
+}
