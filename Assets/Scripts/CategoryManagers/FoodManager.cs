ac01252b6bd72925a70188d0de6c8f23e20e7e70
David854481 <107841443+David854481@users.noreply.github.com>
2022-07-25 15:04:01 +0900

    goodbye google sheets

--- Assets/Scripts/CategoryManagers/FoodManager.cs
+++ Assets/Scripts/CategoryManagers/FoodManager.cs
@@ -1,28 +1,16 @@
 using UnityEngine;
 using System.Collections;
-using GoogleSheetsToUnity;
 using UnityEngine.UI;
 using UnityEngine.Events;
 using System.Collections.Generic;
 
 public class FoodManager : MonoBehaviour
 {
-    [HideInInspector]
-    public string associatedSheet = "1sqOwvXcx39TKr-JUaTyJTMAbIfsk3i6Vw_g5X5px4d8";
-    [HideInInspector]
-    public string associatedWorksheet = "Food";
+    //[HideInInspector]
+    //public string associatedSheet = "1sqOwvXcx39TKr-JUaTyJTMAbIfsk3i6Vw_g5X5px4d8";
+    //[HideInInspector]
+    //public string associatedWorksheet = "Food";
 
     //public List<AnimalObject> animalObjects = new List<AnimalObject>();
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
